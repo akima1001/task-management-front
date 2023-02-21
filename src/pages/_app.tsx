67bfeec2c5bedb0b70bad837441820e9473de32f
mimosa23 <dev.00001001@gmail.com>
2023-02-21 20:36:02 +0900

    update 設定周り

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,30 @@
 import '@/styles/globals.css'
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
+
+const Theme = extendTheme({
+  styles: {
+    global: {
+      '*': {
+        boxSizing: 'border-box',
+        p: 0,
+        m: 0
+      },
+      a: {
+        color: 'inherit',
+        textDecoration: 'none'
+      },
+      body: {
+        maxWidth: '100vw',
+        overflowX: 'hidden'
+      }
+    }
+  }
+})
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={Theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   )
